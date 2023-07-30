1a70aba7bae38f0d996fd6c2fc2888a34af84488
LeeHaoYuan2000 <752295214@qq.com>
2023-07-30 08:51:14 +0800

    keybroad is ok

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,9 +4,9 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int key = inl(KBD_ADDR);
 
- // kbd->keycode = ;
+  kbd->keydown = key & KEYDOWN_MASK;
+  kbd->keycode = key & ~KEYDOWN_MASK;
 
 }
