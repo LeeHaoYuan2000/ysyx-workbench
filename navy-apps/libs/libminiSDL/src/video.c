c1c1c8bded4dfaea6be17e0302037e4dc6f0a496
LeeHaoYuan2000 <752295214@qq.com>
2024-01-10 15:44:36 +0800

    Fix bugs in libminiSDL Video.c

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -11,96 +11,59 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
   
-  // int src_x = 0;int src_y = 0;
-  // int src_w = 0;int src_h = 0;
+  int src_x = 0;int src_y = 0;
+  int src_w = 0;int src_h = 0;
 
-  // int dst_x = 0;int dst_y = 0;
+  int dst_x = 0;int dst_y = 0;
 
-  // if(srcrect == NULL){
-  //     src_x = 0;
-  //     src_y = 0;
-  //     src_w = src->w;
-  //     src_h = src->h;
-  // }
-  // else{
-  //     src_x = srcrect->x;
-  //     src_y = srcrect->y;
-  //     src_w = srcrect->w;
-  //     src_h = srcrect->h;
-  // }
-
-  // if(dstrect == NULL){
-  //     dst_x = 0;dst_y = 0;
-  // }
-  // else{
-  //     dst_x = dstrect->x;
-  //     dst_y = dstrect->y;
-  // }
-  // //copy the pixels to  dst
-  // int cnt_x = 0;
-  // int cnt_y = 0;
-
-  // for(cnt_y = 0; cnt_y < src_h ; cnt_y ++){
-
-  //   if(cnt_y + dst_y > dst->h){
-  //     break;
-  //   }
-
-  //   for(cnt_x = 0; cnt_x < src_w ; cnt_x ++){
-  //       if(cnt_x + dst_x >= dst->w){
-  //         break;
-  //       }
-  //       if(dst->format->BitsPerPixel == 8){
-  //         *(dst->pixels +(dst_x + cnt_x) + (dst_y + cnt_y)*dst->w ) = *(src->pixels + (src_x + cnt_x) + (src_y + cnt_y)*src_w );
-  //       }
-  //       else{
-  //         *((uint32_t*)dst->pixels +(dst_x + cnt_x) + (dst_y + cnt_y)*dst->w ) = *((uint32_t*)src->pixels + (src_x + cnt_x) + (src_y + cnt_y)*src_w ); 
-  //       }
-        
-  //   }
-
-
-  // }
-  //SDL_UpdateRect(dst, 0, 0, 0, 0);
-
-
-   int w,h,dst_x,dst_y,src_x,src_y;
-
-  if(srcrect == NULL){     // entire surface.
-    w = src->w; h = src->h;
-    src_x = 0; src_y = 0;
+  if(srcrect == NULL){
+      src_x = 0;
+      src_y = 0;
+      src_w = src->w;
+      src_h = src->h;
   }
   else{
-    w = srcrect->w; h = srcrect->h;
-    src_x = srcrect->x; src_y = srcrect->y;
+      src_x = srcrect->x;
+      src_y = srcrect->y;
+      src_w = srcrect->w;
+      src_h = srcrect->h;
   }
 
   if(dstrect == NULL){
-    dst_x = 0; dst_y = 0;
+      dst_x = 0;dst_y = 0;
   }
   else{
-    dst_x = dstrect->x; dst_y = dstrect->y;
+      dst_x = dstrect->x;
+      dst_y = dstrect->y;
   }
+  //copy the pixels to  dst
+  int cnt_x = 0;
+  int cnt_y = 0;
 
-  if (dst->format->BitsPerPixel == 32){
-    uint32_t *dst_pixels = (uint32_t *)dst->pixels;
+   uint32_t *dst_pixels = (uint32_t *)dst->pixels;
     uint32_t *src_pixels = (uint32_t *)src->pixels;
-    for(int j = 0; j< h; j++){
-      for(int i = 0; i< w; i++){
-        dst_pixels[(dst_y+j)*(dst->w)+(dst_x+i)] = src_pixels[(src_y+j)*(src->w)+(src_x+i)];
-      }
+
+  for(cnt_y = 0; cnt_y < src_h ; cnt_y ++){
+
+    // if(cnt_y + dst_y > dst->h){
+    //   break;
+    // }
+
+    for(cnt_x = 0; cnt_x < src_w ; cnt_x ++){
+        // if(cnt_x + dst_x >= dst->w){
+        //   break;
+        // }
+        if(dst->format->BitsPerPixel == 8){
+          *(dst->pixels +(dst_x + cnt_x) + (dst_y + cnt_y)*dst->w ) = *(src->pixels + (src_x + cnt_x) + (src_y + cnt_y)*src_w );
+        }
+        else{
+          //*((uint32_t*)(dst->pixels +(dst_x + cnt_x) + (dst_y + cnt_y)*dst->w) ) = *((uint32_t*)(src->pixels + (src_x + cnt_x) + (src_y + cnt_y)*src_w ));
+          dst_pixels[(dst_y+cnt_y)*(dst->w)+(dst_x+cnt_x)] = src_pixels[(src_y+cnt_y)*(src->w)+(src_x+cnt_x)];
+        }
+        
     }
-  }
-  else if (dst->format->BitsPerPixel == 8){
-    for(int j = 0; j< h; j++){
-      for(int i = 0; i< w; i++){
-        dst->pixels[(dst_y+j)*(dst->w)+(dst_x+i)] = src->pixels[(src_y+j)*(src->w)+(src_x+i)];
-      }
-    }
-  }
-  else{
-    printf("SDL_BlitSurface: miniSDL do not support BitsPerPixel == %d.",dst->format->BitsPerPixel);
-    assert(0);
+
+
   }
 
 }
@@ -136,78 +99,36 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     }
   }
 
-  //SDL_UpdateRect(dst, 0, 0, 0, 0);
-
-  // int x,y,w,h;
-  // if(dstrect == NULL){
-  //   x = 0; y = 0; 
-  //   w= dst->w; h = dst->h;
-  // }
-  // else{
-  //   w = dstrect->w; h = dstrect->h;
-  //   x = dstrect->x; y = dstrect->y;
-  // }
-
-  // uint32_t *pixels = (uint32_t *)dst->pixels;
-  // for(int j = 0; j< h; j++){
-  //   for(int i = 0; i< w; i++){
-  //     pixels[(y+j)*(dst->w)+(x+i)] = color;
-  //   }
-  // }
-
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 
-  // if(s->format->BitsPerPixel == 8){
-  //   uint32_t size = 0; 
-  //   if(w == 0 && h == 0){
-  //     size = s->w * s->h;
-  //     uint32_t * pixel = (uint32_t *)malloc(sizeof(uint32_t) * size);
-  //     for(int i = 0; i < size ; i++){
-  //        SDL_Color color = s->format->palette->colors[s->pixels[i]]; 
-  //       pixel[i] = (uint32_t)color.a<<24 || (uint32_t)color.r<<16 || (uint32_t)color.g << 8 || (uint32_t)color.b;
-  //     }
-  //     NDL_DrawRect(s->pixels, x, y, w, h);
-  //     free(pixel);
-  //   }
-  //   else {
-  //     size = w * h;
-  //     uint32_t * pixel = (uint32_t *)malloc(sizeof(uint32_t) * size);
-  //     for(int i = 0; i < size ; i++){
-  //       SDL_Color color = s->format->palette->colors[s->pixels[i]]; 
-  //       pixel[i] = (uint32_t)color.a<<24 || (uint32_t)color.r<<16 || (uint32_t)color.g << 8 || (uint32_t)color.b;
-  //     }
-  //     NDL_DrawRect(s->pixels, x, y, w, h);
-  //     free(pixel);
-  //   }
-
-  // }
-  // else { // BItsPerpixel 32bits
-  //   NDL_DrawRect(s->pixels, x, y, w, h);
-  // }
-
-    if(x == 0 && y == 0 && w == 0 && h == 0){
-    x = 0; y = 0; w= s->w; h = s->h;
-  }
-  
-  if (s->format->BitsPerPixel == 32){
-    NDL_DrawRect((uint32_t*)s->pixels,x,y,w,h);
-  }
-  else if (s->format->BitsPerPixel == 8)
-  {
-    int len = w * h;
-    uint32_t * pixels = (uint32_t *)malloc(4*len);
-    for(int i=0; i<len; i++){
-      SDL_Color pixel = s->format->palette->colors[s->pixels[i]];
-      pixels[i] = (uint32_t)pixel.a<<24 | (uint32_t)pixel.r<<16 | (uint32_t)pixel.g<<8 | (uint32_t)pixel.b;
+  if(s->format->BitsPerPixel == 8){
+    uint32_t size = 0; 
+    if(w == 0 && h == 0){
+      size = s->w * s->h;
+      uint32_t * pixel = (uint32_t *)malloc(sizeof(uint32_t) * size);
+      for(int i = 0; i < size ; i++){
+         SDL_Color color = s->format->palette->colors[s->pixels[i]]; 
+        pixel[i] = (uint32_t)color.a<<24 | (uint32_t)color.r<<16 | (uint32_t)color.g << 8 | (uint32_t)color.b;
+      }
+      NDL_DrawRect(pixel, x, y, w, h);
+      free(pixel);
     }
-    NDL_DrawRect(pixels,x,y,w,h);
-    free(pixels);
+    else {
+      size = w * h;
+      uint32_t * pixel = (uint32_t *)malloc(sizeof(uint32_t) * size);
+      for(int i = 0; i < size ; i++){
+        SDL_Color color = s->format->palette->colors[s->pixels[i]]; 
+        pixel[i] = (uint32_t)color.a<<24 | (uint32_t)color.r<<16 | (uint32_t)color.g << 8 | (uint32_t)color.b;
+      }
+      NDL_DrawRect(pixel, x, y, w, h);
+      free(pixel);
+    }
+
   }
-  else{
-    printf("SDL_UpdateRect: miniSDL do not support BitsPerPixel == %d.",s->format->BitsPerPixel);
-    assert(0);
+  else { // BItsPerpixel 32bits
+    NDL_DrawRect(s->pixels, x, y, w, h);
   }
   
 }
