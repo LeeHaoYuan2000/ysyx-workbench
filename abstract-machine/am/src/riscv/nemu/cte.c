00edf183984d4f09aa470aaf577be74df6095dc1
LeeHaoYuan2000 <752295214@qq.com>
2024-01-20 12:34:55 +0800

    Kcontext part 2 done

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,6 +4,7 @@
 
 #define CONTEXT_SIZE_64bits  ((32 + 3 + 1) * 8)
 #define OFFSET_EPC           (34 * 8)
+#define REG_A0               (10 * 8)
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
@@ -53,6 +54,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   
   void *Context_Start           = end - CONTEXT_SIZE_64bits; //Context is stored in the end of stack 
   *(uint64_t*)(Context_Start + OFFSET_EPC ) = (uint64_t)(entry - 4); // enter the Application after restore the context
+  *(uint64_t*)(Context_Start + REG_A0 )     = (uint64_t)arg;//pass argument th f()
 
   printf("start addr:0x%x   end addr:0x%x  Context_start addr:0x%x  entry_addr :0x%x\n",start ,end ,Context_Start,(char *)(entry - 4));
   return (Context *)Context_Start;
